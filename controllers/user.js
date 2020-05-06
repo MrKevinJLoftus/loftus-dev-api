bd0ca52eda84805b3c93086ff8665106afbeccd5
Kevin Loftus <mrkevinjloftus@gmail.com>
2020-05-05 23:55:16 -0400

    Converting user methods to async

--- controllers/user.js
+++ controllers/user.js
@@ -3,60 +3,52 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const authError = new Error('Your username or password is incorrect.');
 
-exports.userSignUp = (req, res, next) => {
-    // create a new user and store it in the database
-    console.log("creating new user");
-    bcrypt.hash(req.body.password, 15)
-      .then(hash => {
-        const user = new User({
-          username: req.body.username,
-          password: hash
-        });
-        user.save()
-          .then(result => {
-            const token = jwt.sign({username: result.username, userId: result._id},
-              process.env.LOFTUS_DEV_JWT_KEY,
-              { expiresIn: '4h' }
-            );
-            res.status(200).json({
-              token: token,
-              expiresIn: 3600,
-              userId: result._id
-            });
-          })
-          .catch(err => {
-            console.log(err);
-            next(err);
-          });
-      });
+exports.userSignUp = async (req, res) => {
+  // create a new user and store it in the database
+  console.log("creating new user");
+  const hash = await bcrypt.hash(req.body.password, 15);
+  const user = new User({
+    username: req.body.username,
+    password: hash
+  });
+  const saveResult = await user.save();
+  const token = jwt.sign({username: saveResult.username, userId: saveResult._id},
+    process.env.LOFTUS_DEV_JWT_KEY,
+    { expiresIn: '4h' }
+  );
+  res.status(200).json({
+    token: token,
+    expiresIn: 14400,
+    userId: saveResult._id
+  });
 }
 
-exports.userLogin = async (req, res, next) => {
+exports.userLogin = async (req, res) => {
   console.log(`${req.body.username} logging in`);
   const fetchedUser = await User.findOne({ username: req.body.username });
-    if (!fetchedUser) {
-        throw authError;
-    }
-    // found user
-    const hashMatch = await bcrypt.compare(req.body.password, user.password);
-    if (!hashMatch) {
-        throw authError;
-    }
-    // hashes match, correct password entered
-    // time to generate user's JWT
-    const token = jwt.sign(
-        {
-            username: fetchedUser.username, userId: fetchedUser._id
-        },
-        process.env.LOFTUS_DEV_JWT_KEY,
-        {
-            expiresIn: '4h'
-        }
-    );
-    console.log('login successful!');
-    res.status(200).json({
-        token: token,
-        expiresIn: 14400,
-        userId: fetchedUser._id
-    });
+  if (!fetchedUser) {
+    throw authError;
   }
+  // found user
+  const hashMatch = await bcrypt.compare(req.body.password, user.password);
+  if (!hashMatch) {
+    throw authError;
+  }
+  // hashes match, correct password entered
+  // time to generate user's JWT
+  const token = jwt.sign(
+    {
+      username: fetchedUser.username, userId: fetchedUser._id
+    },
+    process.env.LOFTUS_DEV_JWT_KEY,
+    {
+      expiresIn: '4h'
+    }
+  );
+  console.log('login successful!');
+  res.status(200).json({
+    token: token,
+    expiresIn: 14400,
+    userId: fetchedUser._id
+  });
+}
